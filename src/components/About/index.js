3333bd31058ea4b955b6ff694ddf933d8e2fe031
“Danyal <danyalkhanzada89@gmail.com>
2021-07-27 16:17:04 -0400

    added cover photo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,11 @@
+import coverImage from "../../assets/cover/cover-image.jpg";
 import React from 'react';
 
 function About() {
   return (
     <section className="my-5">
         <h1 id="about">Who am I?</h1>
+        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     </section>
   );
 }

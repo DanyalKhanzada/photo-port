b94772009326936c3215675ca8d461a5b6f622d7
“Danyal <danyalkhanzada89@gmail.com>
2021-07-27 16:06:59 -0400

    app.js added about

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import About from './components/About';
 
 function App() {
+
   return (
     <div>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }

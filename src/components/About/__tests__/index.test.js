a4cbc6be3b9091d2841874feb48527ea063ab5b4
“Danyal <danyalkhanzada89@gmail.com>
2021-07-28 17:23:50 -0400

    import

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
